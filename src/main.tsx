6936db3c2ee883eb22503eb332fc5458919d1a63
kk-ahmean <k.k.ahmean@gmail.com>
2025-08-01 16:54:09 +0800

    Fix GitHub Pages deployment: add base path and use HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/locale/zh_CN.js'
 import enUS from 'antd/locale/en_US.js'
@@ -36,8 +36,8 @@ export { LanguageContext };
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppWrapper />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 ) 
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Quality-Manage-System/',
   plugins: [react()],
   resolve: {
     alias: {
